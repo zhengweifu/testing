eda91c8bf98b8fc1f4e9308af7bf1c07506da48b
zhengweifu <zhengweifu@gdi.com.cn>
2016-08-30 18:04:23 +0800

    add ..

--- blend4web/deploy/apps/test01/index.js
+++ blend4web/deploy/apps/test01/index.js
@@ -7,18 +7,33 @@
 
 	var geometry = b4w.require('geometry');
 
-	// console.log(scenes);
+	var objects = b4w.require('objects');
+
+	var textures = b4w.require('textures');
+
+	// console.log(textures);
 
 	app.init({
 		canvas_container_id: 'app',
 		show_fps: true,
+		console_verbose: true,
 		callback: function(canvas, su) {
+			console.log(canvas);
+
 			data.load('index.json', function() {
-				var obj = scenes.get_object_by_name('Cube');
-				console.log(obj);
-				console.log(geometry.extract_vertex_array(obj, 'Material.002', 'a_position'));
+				app.enable_camera_controls(false, false, false, canvas, true);
 				console.log('loading...');
+			}, function(percentage, load_time) {
+				console.log(percentage, load_time);
 			});
 		}
 	});
+
+	document.getElementById('fun-test-btn').addEventListener('click', function(e) {
+		var obj = scenes.get_object_by_name('m_box');
+		var newObj = objects.copy(obj, 'm_box_1', true);
+		console.log(newObj);
+		console.log(textures.get_texture_names(obj));
+		console.log(geometry.extract_vertex_array(obj, 'Material.002', 'a_position'));
+	}, false);
 })();
